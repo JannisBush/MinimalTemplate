e96b58a0878aa72b5e7c3f7811e3cefd1dc31022
Jannis Bush <jrautenstrau@uni-osnabrueck.de>
2018-06-07 18:58:03 +0200

    update analyse script, two models

--- analyses/explore_data.r
+++ analyses/explore_data.r
@@ -3,7 +3,7 @@ library(tidyverse)
 setwd("where the data is")
 
 # read the data
-d = readr::read_csv('name of the datafile.csv') %>% 
+d = readr::read_csv('name of data.csv') %>% 
   # only look at main trials
   filter(block != "practice") %>%
   # kick out all participants with less than 85% correct in the main trials
@@ -19,53 +19,82 @@ d = readr::read_csv('name of the datafile.csv') %>%
   # get the main conditions valid_cue vs invalid_cue 
   mutate(conditionCue = factor(case_when( orgPrime == targetPrime ~ "valid_cue",
                                             TRUE ~ "invalid_cue"), ordered = T, levels = c("valid_cue", "invalid_cue"))) %>%
-  # divide invalid_cue in between_object and within_object
-  mutate(conditionShift = factor(case_when( orgPrime == targetPrime ~ "valid_cue",
-                                         (rotate == "false" & abs(orgPrime - targetPrime) > 1) | (rotate == "true" & abs(orgPrime - targetPrime) == 1) ~ "within_object",
-                                         TRUE ~ "between_object"), ordered = T, levels = c("valid_cue", "within_object", "between_object"))) %>%
-  # divide invalid_cue in between_field and within_field
-  mutate(conditionField = factor(case_when( orgPrime == targetPrime ~ "valid_cue",
-                                            (rotate == "false" & conditionShift == "within_object") | (rotate == "true" & conditionShift == "between_object") ~ "within_field",
-                                            TRUE ~ "between_field"), ordered = T, levels = c("valid_cue", "within_field", "between_field")))  #%>%
-
+  # get the condition left vs right visual field
+  mutate(conditionField = factor(case_when((targetPrime == 0 | targetPrime == 4) ~ "left_field",
+                                           TRUE ~ "right_field"), ordered = T, levels = c("left_field", "right_field"))) %>%
+  # get the condition horizontal vs vertical orientation
+  mutate(conditionOrientation = factor(case_when(rotate == "false" ~ "vertical",
+                                                 TRUE ~ "horizontal"), ordered = T, levels = c("horizontal", "vertical")))
   # TODO kick out all trials faster than 3 std from mean for all 3 conditions
-
+  
   # TODO kick out all trials slower than 3 std from mean for all 3 conditions
   
   # TODO other stuff?
 
 # Summarize the RTs
-dsummary = d %>% group_by(conditionCue, conditionShift, conditionField) %>% 
+dsummary = d %>% group_by(conditionCue, conditionField, conditionOrientation) %>% 
   summarize(meanRT = mean(RT)) %>% 
   ungroup() 
 dsummary
 
-  
 # plot condition valid cue vs invalid cue
-ggplot(d , aes(y = log(RT), x = conditionCue)) + geom_violin()
-ggplot(d , aes(x = log(RT), color = conditionCue)) + geom_density()
+ggplot(d, aes(y = log(RT), x = conditionCue)) + geom_violin()
+ggplot(d, aes(x = log(RT), color = conditionCue)) + geom_density()
 
-# plot condition invalid cues: within_object vs between_object
-ggplot(filter(d,conditionCue == "invalid_cue") , aes(y = log(RT), x = conditionShift)) + geom_violin()
-ggplot(filter(d,conditionCue == "invalid_cue") , aes(x = log(RT), color = conditionShift)) + geom_density()
+# plot condition left_field vs right_field
+ggplot(d, aes(y = log(RT), x = conditionField)) + geom_violin()
+ggplot(d, aes(x = log(RT), color = conditionField)) + geom_density()
 
-# plot condition invalid cues: within_field vs between_field
-ggplot(filter(d,conditionCue == "invalid_cue") , aes(y = log(RT), x = conditionField)) + geom_violin()
-ggplot(filter(d,conditionCue == "invalid_cue") , aes(x = log(RT), color = conditionField)) + geom_density()
+# plot condition horizontal vs vertical orientation
+ggplot(d, aes(y = log(RT), x = conditionOrientation)) + geom_violin()
+ggplot(d, aes(x = log(RT), color = conditionOrientation)) + geom_density()
 
-# do a linear model to predict log RT valid_cue vs invalid_cue
-mod = lm(log(RT) ~ conditionCue, data = d)  
-summary(mod)
+# do a linear model to predict log RT valid_cue vs invalid_cue, left vs right and horizontal vs vertical
+modLM = lm(log(RT) ~ conditionCue + conditionField + conditionOrientation, data = d)  
+summary(modLM)
 
-# do a linear model to predict log RT invalid_cue: within_object vs between_object
-mod = lm(log(RT) ~ conditionShift, data = filter(d, conditionCue == "invalid_cue"))  
-summary(mod)
+# do a three-way ANOVA to predict log RT valid_cue vs invalid_cue, left vs right and horizontal vs vertical
+modAOV = aov(log(RT) ~ conditionCue + conditionField + conditionOrientation, data = d)  
+summary(modAOV)
 
-# do a linear model to predict log RT invalid_cue: within_field vs between_field
-mod = lm(log(RT) ~ conditionField, data = filter(d, conditionCue == "invalid_cue"))  
-summary(mod)
+######################SECOND ANALYSIS ONLY INVALID CUES###########################
 
-# do a linear model to say that rotation does not make a difference
-mod = lm(log(RT) ~ rotate, data = d)  
-summary(mod)
+# second analysis only for the invalid cues 
+d_invalid = d %>% 
+  # TODO subtract mean of the valid_cue condition from the invalid_cue condition
+  
+  # kick out all valid_cues
+  filter(conditionCue == "invalid_cue") %>%
+  # divide invalid_cue in between_object (cued) and within_object (uncued)
+  mutate(conditionRectangle = factor(case_when((rotate == "false" & abs(orgPrime - targetPrime) > 1) | (rotate == "true" & abs(orgPrime - targetPrime) == 1) ~ "within_object",
+                                           TRUE ~ "between_object"), ordered = T, levels = c("within_object", "between_object"))) %>%
+# divide invalid_cue in between_field (horizontal) and within_field (vertical)
+mutate(conditionShift = factor(case_when((rotate == "false" & conditionRectangle == "within_object") | (rotate == "true" & conditionRectangle == "between_object") ~ "vertical_shift",
+                                         TRUE ~ "horizontal_shift"), ordered = T, levels = c("vertical_shift", "horizontal_shift")))  
+
+# Summarize the RTs
+d_invalid_summary = d_invalid %>% group_by(conditionRectangle, conditionField, conditionShift) %>% 
+  summarize(meanRT = mean(RT)) %>% 
+  ungroup() 
+d_invalid_summary
+
+# plot condition between_object vs within_object
+ggplot(d_invalid, aes(y = log(RT), x = conditionRectangle)) + geom_violin()
+ggplot(d_invalid, aes(x = log(RT), color = conditionRectangle)) + geom_density()
+
+# plot condition left_field vs right_field
+ggplot(d_invalid, aes(y = log(RT), x = conditionField)) + geom_violin()
+ggplot(d_invalid, aes(x = log(RT), color = conditionField)) + geom_density()
+
+# plot condition horizontal vs vertical shift
+ggplot(d_invalid, aes(y = log(RT), x = conditionShift)) + geom_violin()
+ggplot(d_invalid, aes(x = log(RT), color = conditionShift)) + geom_density()
+
+# do a linear model to predict log RT between_object vs within_object, left_field vs right_field and horizontal_shift vs vertical_shift
+modInvalidLM = lm(log(RT) ~ conditionRectangle + conditionField + conditionShift, data = d_invalid)  
+summary(modInvalidLM)
+
+# do a three-way ANOVA to predict log RT between_object vs within_object, left_field vs right_field and horizontal_shift vs vertical_shift
+modInvalidAOV = aov(log(RT) ~ conditionRectangle + conditionField + conditionShift, data = d_invalid)  
+summary(modInvalidAOV)
 
